ba62d8144e088bab846e24e21fad3b373f6be9aa
yuanjq <yuanjq91@gmail.com>
2013-03-19 09:16:10 +0800

    change

--- src/ic_input_toolbar.h
+++
@@ -1,8 +0,0 @@
-#ifndef IC_INPUT_TOOLBAR_H
-#define IC_INPUT_TOOLBAR_H
-
-#include <gtk/gtk.h>
-
-GtkWidget *ic_input_toolbar_new();
-
-#endif
--- src/ic_toolbar.h
+++
@@ -1,8 +0,0 @@
-#ifndef IC_TOOLBAR_H
-#define IC_TOOLBAR_H
-
-#include <gtk/gtk.h>
-
-GtkWidget *ic_tool_bar_new();
-
-#endif
